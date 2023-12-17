b4e45b9eaf63ab0ffa306ad4ce6bb275c31424ed
cristianiniguez <felizpc1@gmail.com>
2023-12-17 23:09:50 +0000

    chakra-ui installed

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
+import { ChakraProvider } from '@chakra-ui/react';
 import { Navigate, RouterProvider, createBrowserRouter } from 'react-router-dom';
 
 const router = createBrowserRouter([
   { path: '/', element: <Navigate to='/controls' /> },
   { path: '/controls', element: <h1>Here should be the controls</h1> },
   { path: '/song', element: <h1>Here should be the random song</h1> },
-])
+]);
 
 function App() {
   return (
-    <RouterProvider router={router} />
+    <ChakraProvider>
+      <RouterProvider router={router} />
+    </ChakraProvider>
   );
 }
 
