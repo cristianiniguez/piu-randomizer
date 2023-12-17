9f00114ae3860e95106400d5a4583a28dc1e1d5a
cristianiniguez <felizpc1@gmail.com>
2023-12-17 23:56:18 +0000

    formik installed and created game edition field

--- src/pages/FormPage.tsx
+++ src/pages/FormPage.tsx
@@ -1,5 +1,40 @@
+import { Field, Form, Formik, type FormikProps } from 'formik';
+import Layout from '@/components/Layout';
+import { Button, FormControl, FormLabel, Select, VStack } from '@chakra-ui/react';
+
+type FormValues = {
+  gameEdition: string;
+};
+
 const FormPage = () => {
-  return <div>FormPage</div>;
+  const renderForm = ({ isSubmitting }: FormikProps<FormValues>) => {
+    return (
+      <Form>
+        <VStack alignItems='stretch' spacing={4}>
+          <FormControl>
+            <FormLabel htmlFor='gameEdition'>Game Edition</FormLabel>
+            <Field as={Select} id='gameEdition' name='gameEdition'>
+              <option value='piu-phoenix'>PIU Phoenix</option>
+            </Field>
+          </FormControl>
+
+          <Button colorScheme='blue' isLoading={isSubmitting} type='submit'>
+            Search
+          </Button>
+        </VStack>
+      </Form>
+    );
+  };
+
+  const getInitialValues = (): FormValues => ({
+    gameEdition: 'piu-phoenix',
+  });
+
+  return (
+    <Layout>
+      <Formik component={renderForm} initialValues={getInitialValues()} onSubmit={console.log} />
+    </Layout>
+  );
 };
 
 export default FormPage;
