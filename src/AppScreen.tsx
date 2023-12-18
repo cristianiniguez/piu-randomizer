8d3e75aa8c65dc01e82e802166c7eac624312f55
cristianiniguez <felizpc1@gmail.com>
2023-12-18 01:15:20 +0000

    Removed shadcn stuff

--- src/AppScreen.tsx
+++
@@ -1,7 +0,0 @@
-import type { FC, PropsWithChildren } from 'react';
-
-const AppScreen: FC<PropsWithChildren> = ({ children }) => {
-  return <section className='h-screen p-8 grid place-items-stretch'>{children}</section>;
-};
-
-export default AppScreen;
